0a56a4de82e34f9749105ab1c640e4076170fc24
Luiz Henrique <7henrique18@gmail.com>
2025-02-26 12:58:59 -0300

    feat: Enhance home page layout with UI improvements and logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
 import { logout } from "./actions";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>logado.</h1>
-      
-      <form action={logout}>
-        <button type="submit">Logout</button>
-      </form>
-    </div>
-  );
+	return (
+		<main className="py-16 h-screen">
+			<section className="p-6 border border-dashed h-full rounded-lg max-w-2xl mx-auto bg-white">
+				<div className="flex justify-between">
+					<div className="flex items-center gap-2">
+						<h1 className="text-2xl font-bold">My notes</h1>
+
+						<form action={logout}>
+							<Button type="submit" variant="outline">
+								Logout
+							</Button>
+						</form>
+					</div>
+
+					<div className="flex items-center gap-2">
+						<Button>
+							<PlusIcon className="size-4" />
+							New note
+						</Button>
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 }
